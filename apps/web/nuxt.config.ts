d56408a1a8425c225974d6eced3c48ea67852c24
Akash <aakash@hey.com>
2023-01-29 10:50:44 +0530

    update packages

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,4 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  ssr: false,
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "nuxt-headlessui",
+  ],
+  css: ["@fontsource/inter/variable.css"],
+  typescript: {
+    strict: true,
+  },
+  vite: {
+    build: {
+      target: "esnext",
+    },
+  },
+  nitro: {
+    esbuild: {
+      options: {
+        target: "esnext",
+      },
+    },
+  },
+  app: {
+    keepalive: true,
+    head: {
+      // Prevent arbitrary zooming on mobile devices
+      viewport:
+        "width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover",
+      bodyAttrs: {
+        class: "overflow-x-hidden",
+      },
+    },
+  },
 })
